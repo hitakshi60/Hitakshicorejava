e9275f757a0c24b4d01e1e444f103430299cdee3
Dhupesh Trivedi <Dhupesh Trivedi@HITUDHUPU>
2024-06-27 12:59:06 +0530

    Day1 Programs

---
+++ secondprogram/src/com/wiect/secp/IfElse.java
@@ -0,0 +1,17 @@
+
+package com.wiect.secp;
+
+public class IfElse {
+
+    public static void main(String[] args) {
+        int i = 1;
+        int j = 10;
+        for(j=10;j<=20;j++) {
+        for(i=1;i<=10;i++) {
+        	System.out.println(j+"X"+i+"=" +(j*i));
+        }
+    } 
+  }
+}
+
+
